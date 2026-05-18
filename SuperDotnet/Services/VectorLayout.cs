573abec29734fa2224a8e31a8fc33044a9eb97e9
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2026-05-18 15:29:09 -0300

    Clean submission branch: only deploy files + Docker Hub image

--- SuperDotnet/Services/VectorLayout.cs
+++
@@ -1,9 +0,0 @@
-namespace SuperDotnet.Services;
-
-public static class VectorLayout
-{
-    public const int Dimensions = 14;
-
-    // High-variance/sentinel dimensions first so scalar early-abort cuts sooner.
-    public static ReadOnlySpan<int> SpecDimensionOrder => [5, 6, 2, 7, 8, 9, 10, 11, 12, 0, 1, 3, 4, 13];
-}
